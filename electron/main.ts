eed209855f2553ca5874753ea0fda828e730b9a6
tamerugur <tamerugur3@hotmail.com>
2025-02-28 12:11:31 +0300

    initial commit, created boilerplate for electron+react

---
+++ electron/main.ts
@@ -0,0 +1,24 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadURL("http://localhost:5173");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
